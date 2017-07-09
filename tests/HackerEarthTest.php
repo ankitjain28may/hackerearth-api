e3a1f48614717a23e8cbc1167555b85a73443829
ankitjain28may <ankitjain28may77@gmail.com>
2017-07-10 01:11:57 +0530

    Write first test

--- tests/HackerEarthTest.php
+++ tests/HackerEarthTest.php
@@ -3,16 +3,25 @@
 namespace Ankitjain28may\HackerEarth\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Ankitjain28may\HackerEarth\HackerEarth;
+
 
 class HackerEarthTest extends TestCase
 {
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testExample()
-    {
-        $this->assertTrue(true);
+    public function testCompile()
+    {	
+    	$config =[
+    		"api_key" => "asdfghjkl"
+    	];
+    	$ob = new HackerEarth($config);
+
+    	$result = $ob->Compile();
+        $this->assertEquals($result, '{"message":"Invalid Input"}');
     }
 }
