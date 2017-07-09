176711005a2e2d84dc297b00c61eee6590861c17
ankitjain28may <ankitjain28may77@gmail.com>
2017-07-09 23:46:02 +0530

    Writing tests

---
+++ tests/HackerEarthTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ankitjain28may\HackerEarth\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class HackerEarthTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
